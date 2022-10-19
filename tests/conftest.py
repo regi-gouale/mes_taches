69e55b6aaf8658ab2230f4f286688497a20a3f86
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2022-10-19 19:45:11 +0200

    Setting up selenium webdriver

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+"""
+This module contains the fixtures for the tests.
+"""
+import pytest
+import selenium.webdriver as webdriver
+
+@pytest.fixture
+def browser():
+    """Fixture pour le navigateur."""
+
+    # On crée une instance de Firefox
+    driver = webdriver.Firefox()
+    
+    # On attend 10 secondes pour que la page se charge
+    driver.implicitly_wait(10)
+    
+    # On retourne le driver pour qu'il soit utilisé par les tests
+    yield driver
+    
+    # On ferme le navigateur
+    driver.quit()
---
+++ tests/test_register.py
@@ -0,0 +1,39 @@
+"""
+Ces tests couvrent l'inscription d'un utilisateur sur le site.
+"""
+
+def test_register_to_site(browser):
+    """
+    Teste l'inscription d'un utilisateur sur le site.
+    """
+    # On se rend sur la page d'inscription
+    # TODO
+    
+    # On entre le nom d'utilisateur
+    # TODO
+
+    # On entre le pseudo de l'utilisateur
+    # TODO
+
+    # On entre l'adresse email de l'utilisateur
+    # TODO
+
+    # On entre le mot de passe de l'utilisateur
+    # TODO
+
+    # On entre la confirmation du mot de passe de l'utilisateur
+    # TODO
+
+    # On clique sur le bouton d'inscription
+    # TODO
+
+    # On vérifie que l'utilisateur est bien redirigé vers la page d'accueil
+    # TODO
+    
+    # On vérifie que l'utilisateur est bien connecté
+    # TODO
+
+    # On vérifie que le message de succès est bien affiché
+    # TODO
+
+    raise NotImplementedError
