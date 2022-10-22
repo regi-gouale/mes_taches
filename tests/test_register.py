f964144af7eb5d95008414d9ec5b9e8cff4357e0
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2022-10-22 11:54:24 +0200

    Multi testing execution Reconfigurer les dossiers Ajout de la configuration de plusieurs tests en parallèle

--- tests/test_register.py
+++ tests/test_register.py
@@ -5,36 +5,36 @@ Ces tests couvrent l'inscription d'un utilisateur sur le site.
 from pages.register import MesTachesRegisterPage
 from pages.home import MesTachesHomePage
 import random
+import pytest
 
-def test_register_to_site(browser):
+@pytest.mark.parametrize("name, username, email, password, password_confirm", [
+    ("Jean Bodard", "jbd", "jbd@boada.com", "AZbci124", "AZbci124"),
+    ("Toto", "toto", "toto@fin.com", "GIbvieooez", "GIbvieooez"),
+])
+def test_register_to_site(browser, name, username, email, password, password_confirm):
     """
     Teste l'inscription d'un utilisateur sur le site.
     """
     register_page = MesTachesRegisterPage(browser)
     home_page = MesTachesHomePage(browser)
 
-    NAME = "Jean Dubois"
-    USERNAME = f"jdubois-{random.randint(1, 999999)}"
-    EMAIL = f"jean.dubois.{random.randint(1, 999999)}@monmail.com"
-    PASSWORD = "123456"
-
     # On se rend sur la page d'inscription
     register_page.load()
     
     # On entre le nom d'utilisateur
-    register_page.fill_name(NAME)
+    register_page.fill_name(name=name)
 
     # On entre le pseudo de l'utilisateur
-    register_page.fill_username(USERNAME)
+    register_page.fill_username(username=username)
 
     # On entre l'adresse email de l'utilisateur
-    register_page.fill_email(EMAIL)
+    register_page.fill_email(email=email)
 
     # On entre le mot de passe de l'utilisateur
-    register_page.fill_password(PASSWORD)
+    register_page.fill_password(password=password)
 
     # On entre la confirmation du mot de passe de l'utilisateur
-    register_page.fill_password_confirm(PASSWORD)
+    register_page.fill_password_confirm(password_confirm=password_confirm)
 
     # On clique sur le bouton d'inscription
     register_page.submit()
@@ -43,7 +43,7 @@ def test_register_to_site(browser):
     assert home_page.is_browser_on_page()
     
     # On vérifie que l'utilisateur est bien connecté
-    assert home_page.is_logged(USERNAME)
+    assert home_page.is_logged(username=username)
 
     # On vérifie que le message de succès est bien affiché
     assert home_page.get_success_message()
